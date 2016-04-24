a1215ff1660d40d91ac7c489d5a120fa08f37c6c
Peter Harpending <peter@harpending.org>
2016-04-24 11:36:07 -0600

    Add silly option parser to main.hs

--- bin/Main.hs
+++ bin/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Options.Applicative
+
 main :: IO ()
-main = return ()
+main =
+  do result <- customExecParser nicePrefs emptyParser
+     print result
+  where
+    emptyParser = infoHelper (pure NoResult) mempty
+    nicePrefs = defaultPrefs { prefDisambiguate = True
+                             , prefShowHelpOnError = True
+                             }
+
+data Result = NoResult
+  deriving Show
+
+infoHelper :: Parser a -> InfoMod a -> ParserInfo a
+infoHelper x = info (helper <*> x)
