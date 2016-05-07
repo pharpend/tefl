a670d5b981f6b44592a869e393f5750359692f07
Peter Harpending <peter@harpending.org>
2016-05-07 14:23:39 -0600

    Add undefined function 'encrypt'.

--- src/Crypto/Tefl.hs
+++ src/Crypto/Tefl.hs
@@ -1,1 +1,8 @@
 module Crypto.Tefl where
+
+import Codec.Archive.Tar
+import System.IO.Temp
+
+-- |For now, just creates a tarball with no compression
+encrypt :: FilePath -> FilePath -> IO ()
+encrypt = undefined
